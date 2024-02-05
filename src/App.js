832d78222b165e078a8f3b9dc38c7efe6d8844b4
John A <kayak.stated_0b@icloud.com>
2024-02-05 00:49:09 +0000

    Started on Teams page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/TopBar"
 import Sidebar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -32,9 +32,9 @@ function App() {
             <main className="content">
               <Topbar setIsSidebar={setIsSidebar} />
               <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
                 {
-                  <Route path="/" element={<Dashboard />} />
-                  // <Route path="/team" element={<Team />} />
                   //<Route path="/contacts" element={<Contacts />} />
                   //<Route path="/invoices" element={<Invoices />} />
                   //<Route path="/form" element={<Form />} />
