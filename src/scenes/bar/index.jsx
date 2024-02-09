8f05985b6d35d005c4d96d3dfbdf15412f4e7f5b
John A <kayak.stated_0b@icloud.com>
2024-02-09 00:19:49 +0000

    Bar Chart section start

---
+++ src/scenes/bar/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+
+const Bar = () => {
+    return(
+        <Box>
+            <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+            <Box>
+                <h1>Bar Chart</h1>
+            </Box>
+        </Box>
+    )
+}
+
+export default Bar;
