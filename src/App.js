0192410b974f6c37f7b72c71ff352d6462e65a03
John A <kayak.stated_0b@icloud.com>
2024-02-10 21:29:25 +0000

    Completed Calendar scene and initally started on Bar Chart.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Form from "./scenes/form";
 // import Geography from "./scenes/geography";
 // import { CssBaseline, ThemeProvider } from "@mui/material";
 // import { ColorModeContext, useMode } from "./theme";
-// import Calendar from "./scenes/calendar/calendar";
+ import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colormode] = useMode();
@@ -37,16 +37,16 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
-                <Route path="/bar" element={<Bar />} />
+                <Route path="/calendar" element={<Calendar />} />
                 {
                   
                   
                   
-                  
+                  //<Route path="/bar" element={<Bar />} />
                   //<Route path="/pie" element={<Pie />} />
                   //<Route path="/line" element={<Line />} />
                   //<Route path="/faq" element={<FAQ />} />
-                  //<Route path="/calendar" element={<Calendar />} />
+                  
                   //<Route path="/geography" element={<Geography />} />
                 }
               </Routes>
--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,13 +1,14 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
+//import BarChart from "../../components/Barchart";
 
 
 const Bar = () => {
     return(
         <Box>
             <Header title="Bar Chart" subtitle="Simple Bar Chart" />
-            <Box>
-                <h1>Bar Chart</h1>
+            <Box height="75vh">
+              <h1> Bar Chat Component goes here</h1>
             </Box>
         </Box>
     )
